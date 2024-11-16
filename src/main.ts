6e7360bf0ae23d69bb5dcf096b217a9ec340b5a3
smtaha512 <s.m.taha10@gmail.com>
2024-11-17 03:01:34 +0500

    refactor: get `port` from config instead of `process.env`

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ApiDocsConfig } from './infra/config/api-docs.config';
-
-const PORT = process.env.PORT!;
+import { EnvConfig, EnvConfigInjectionKey } from './infra/config/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const { port } = app.get<EnvConfig>(EnvConfigInjectionKey);
+
   ApiDocsConfig.setupApiDocs(app);
 
-  await app.listen(PORT);
+  await app.listen(port);
 
-  Logger.debug(`Application running at PORT ${PORT}`, 'main');
+  Logger.debug(`Application running at PORT ${port}`, 'main');
 }
 bootstrap();
